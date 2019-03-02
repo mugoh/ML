59fe3e28c4210969194d193c69226a20c1e861f2
root <mugoh.ks@gmail.com>
2019-03-02 10:30:07 +0300

    Predict and display result

--- poly_regression.py
+++ poly_regression.py
@@ -4,7 +4,7 @@ import pandas
 from .utils import train_test_split, fold_val_sets, mean_square_error
 
 
-def main():
+def regress_polynomial():
     data = pandas.read_csv('path/to/data/', sep='')
     period = numpy.atleast_2d(data['time'].values).T
     temperature = data['temp'].values
@@ -29,7 +29,7 @@ def main():
         cross_val_sets = fold_val_sets(X_train, Y_train, count)
 
         mse = 0
-
+        print("Getting regularization const")
         for _Xtrain, _Xtest, _Ytrain, _Ytest in cross_val_sets:
             data_model = PolynomialRRegression(
                 degree=degree,
@@ -41,3 +41,42 @@ def main():
             _mse = mean_square_error(_Ytest, _Y_prediction)
             mse += _mse
         mse /= count
+        print(f"Mean square root error: {mse} (regularization {reg_factor})")
+
+        # Save constant with lowest error
+
+        if mse < lowest_err:
+            highest_reg_fact = reg_factor
+            lowest_err = mse
+
+        # Final prediction
+        pred_model = PolynomialRRegression(degree,
+                                           highest_reg_fact,
+                                           0.001,
+                                           10000)
+        model.fit(X_train, Y_train)
+        Y_prediction = pred_model.predict(X_test)
+
+        mse = mean_square_error(Y_test, Y_prediction)
+        print(f"Mean Squaredd Error: {lowest_err} from {highest_reg_fact}")
+
+        Y_prediction_line = pred_model.predict(X)
+
+        # Plot data
+        colour_map = plot.get_cmap('viridis')
+        map1 = plot.scatter(366 * X_train, Y_train,
+                            color=colour_map(0.9), s=10)
+        map2 = plot.scatter(366 * X_test, Y_test, color=colour_map(0.5), s=10)
+        plot.plot(366 * X, Y_prediction_line, color='black',
+                  linewidth=2, label='Prediction')
+        plot.suptitle("Ploynomial Regression (Rg)")
+        plot.title("MSE {mse}", fontsize=10)
+        plot.xlabel = ('Day')
+        plot.ylabel('Temperature in Celcius')
+        plot.legend((map1, map2), ("Training Data",
+                                   "Test Data"), loc='lower right')
+        plot.show()
+
+
+if __name__ == '__main__':
+    regress_polnomial()
