5e1f951e472668980d2b324af6455bf37fef94c0
root <mugoh.ks@gmail.com>
2019-03-14 01:52:24 +0300

    Implement cross entropy loss function

--- mlearning/helpers/deep_learning/loss.py
+++ mlearning/helpers/deep_learning/loss.py
@@ -1,3 +1,17 @@
 """
     This module contains deep learning loss functions
 """
+
+import numpy
+
+
+class CrossEntropyLoss:
+    """
+        Perform operationd that aid in computing the
+        cross entropy loss between predicted and true data values.
+    """
+
+    def compute_loss(self, y_true, y_prediction):
+        pred = numpy.clip(y_prediction, 1e-15, 1 - 1e-15)
+
+        return -y_true * numpy.log(pred) - (1 - y_true) * numpy.log(1 - pred)
