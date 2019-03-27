0a1adb8d29282e4279d46dfbb54b0495d93a0c24
mugoh <mugoh.ks@gmail.com>
2019-03-27 19:10:29 +0300

    Create the plot helper class

--- mlearning/helpers/utils/display.py
+++ mlearning/helpers/utils/display.py
@@ -4,6 +4,11 @@
     to the command line.
 """
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import numpy as np
+
 import progressbar
 
 progress_bar_widgets = [
@@ -16,3 +21,30 @@ progress_bar_widgets = [
     ' ',
     progressbar.ETA(), ' '
 ]
+
+
+class Plot:
+    """
+        A data plot display class
+    """
+
+    def __init__(self):
+        self.cmap = plt.get_cmap('viridis')
+
+    def transform(self, X, dimension):
+        """
+            Transforms a given layer to s specified dimension
+        """
+
+        covariance = get_matrix_covariance(X)
+        eigen_values, eigen_vectors = np.linalg.eig(covariance)
+
+        # Sort eigen values ande eigen_vectors by largest eigen values
+        index = eigen_values.argsort()[::-1]
+        eigen_values = eigen_values[index][:dimension]
+        eigen_vectors = np.atleast_1d(eigen_vectors[:, index])[:, :dimension]
+
+        # Project onto principal components
+        transformed_X = X.dot(eigen_vectors)
+
+        return transformed_X
