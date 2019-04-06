fix(output): Fix gradient of loss functions

mlearning/helpers/deep_learning/loss.py
@@ -35,5 +35,5 @@ class CrossEntropyLoss:
         """
             Finds the gradient between p, and q values H(p, q)
         """
-        y_clipped = np.clip(y_pred, 1e-15, 1 - 1e15)
+        y_clipped = np.clip(y_pred, 1e-15, 1 - 1e-15)
         return - (y_true / y_clipped) + (1 - y_true) / (1 - y_clipped)

mlearning/helpers/utils/display.py
@@ -96,6 +96,8 @@ class Plot:
         """
         class_distr = []
         X_transformed = self.transform(X, dimension=2)
+        print(X_transformed)
+        raise Exception
         x_one = X_transformed[:, 0]
         x_two = X_transformed[:, 1]
 
@@ -104,9 +106,9 @@ class Plot:
 
         # Plot class distrbutions
         for num, item in enumerate(np.unique(y)):
-            x_one_ = x_one[y == 1]
-            x_two_ = x_two[y == 1]
-            y_ = y[y == 1]
+            x_one_ = x_one[y == item]
+            x_two_ = x_two[y == item]
+            y_ = y[y == item]
             class_distr.append(plt.scatter(x_one_, x_two_, color=colors[num]))
 
         # Plot legend

mlearning/scripts/convolutional_neural_network.py
@@ -50,10 +50,9 @@ class CNN:
 
         self.add_layers()
 
-        self.classifier.show_model_details('Convolution Network')
+        # self.classifier.show_model_details('Convolution Network')
         training_err, validation_err = self.classifier.fit(
             X_train, y_train, no_of_epochs=50, batch_size=256)
-
         count = len(training_err)
         self.training, = plot.plot(range(count),
                                    training_err,
@@ -103,7 +102,7 @@ class CNN:
         plot.title('Error Plot')
         plot.ylabel('error')
         plot.xlabel('No. of iterations')
-        plot.show()
+        # plot.show()
 
         print('\n\nGetting Accuracy...\n')
         _, accuracy = self.classifier.test_on_batch(X_test, y_test)
@@ -111,6 +110,7 @@ class CNN:
 
         y_prediction = np.argmax(
             self.classifier.make_prediction(X_test), axis=1)
+
         X_test = X_test.reshape(-1, 8 * 8)
 
         # Flatten dimension to Two-D