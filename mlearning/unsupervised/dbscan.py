5d7c2c17a1a9785c948a28176fb5d0f2952d01c5
mugoh <mugoh.ks@gmail.com>
2019-04-25 10:49:17 +0300

    (dbscan): cluster samples

--- mlearning/unsupervised/dbscan.py
+++ mlearning/unsupervised/dbscan.py
@@ -36,6 +36,16 @@ class DBScan:
         for sample in range(unvisited_samples):
             self.neighbouring_s[sample] = self.find_sample_neighbours(sample)
 
+            if len(self.neighbouring_s[sample]) >= self.min_samples:
+                # neighbour is a core point
+                # expand cluster from neighbour
+                self.clusters.append[
+                    self.expand_cluster(sample,
+                                        self.neighbouring_s[sample])
+                ]
+
+        return self.get_cluster_index
+
     def find_sample_neighbours(self, sample_index):
         """
             Gives indexes of samples within a radius of epsilon to
@@ -50,3 +60,42 @@ class DBScan:
             dist = op.get_eucledian_distance(self.X[sample_index], smpl)
             neighbours.append(i) if dist < self.eps else None
         return np.array(neighbours)
+
+    def expand_cluster(self, smpl_indx, neighbours):
+        """
+            Expands the cluster of a dense area until
+            the border is arrived at
+        """
+        cluster = [smpl_indx]
+
+        for ngbr_index in neighbours:
+            if ngbr_index in self.visited_samples:
+                continue
+
+            self.neighbouring_s[ngbr_index] = self.find_sample_neighbours(
+                ngbr_index)
+            self.visited_samples.append(ngbr_index)
+
+            if len(self.neighbouring_s[ngbr_index]) > self.min_samples:
+                cluster += self.expand_cluster(
+                    ngbr_index,
+                    self.neighbouring_s[ngbr_index]
+                )
+            else:
+                cluster.append(ngbr_index)
+
+        return cluster
+
+    def get_cluster_index(self):
+        """
+            Gives the index in which samples are contained
+
+            Outliers have a default index value equal to the number
+            of samples
+        """
+        indices = np.full(shape=self.X.shape[0], fill_value=len(self.clusters))
+
+        for indx, cluster_ in enumerate(self.clusters):
+            for sample_indx in cluster_:
+                indices[sample_indx] = indx
+        return indices
