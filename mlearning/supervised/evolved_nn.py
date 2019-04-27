dd2d4d35fcdad182d67afc02f03c5fea3cc73c65
mugoh <mugoh.ks@gmail.com>
2019-04-27 12:35:52 +0300

    (evolved-nn): initialize model class

---
+++ mlearning/supervised/evolved_nn.py
@@ -0,0 +1,28 @@
+"""
+    Neuroevolution Model
+"""
+
+
+class EvolvedNN:
+    """
+        Evolutionary optimized Neural Network model
+
+        Parameters:
+        ----------
+        X: array_like
+            X training dataset
+        y: array_like
+            Dataset labels
+        n_individuals: int
+            Number of neural networks to form the population
+        mutation_rate: float
+            Probability of mutating a given network weight
+    """
+
+    def __init__(self, X, y,
+                 n_individuals=1000,
+                 mutation_rate=0.01):
+        self.X = X
+        self.y = y
+        self.mtn_rate = mutation_rate
+        self.plt_size = n_individuals
