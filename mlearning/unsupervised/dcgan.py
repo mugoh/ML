b86e65ae10ad70ce9aa9d0c041f64c9cd45e3caf
mugoh <mugoh.ks@gmail.com>
2019-05-26 15:11:16 +0300

    (dcgan): create model generator

--- mlearning/unsupervised/dcgan.py
+++ mlearning/unsupervised/dcgan.py
@@ -2,6 +2,12 @@
     Deep Convolitional Generative Adversarial Network
 """
 
+from ..helpers.deep_learning.network import Neural_Network
+from ..helpers.deep_learning.loss import CrossEntropyLoss
+from ..helpers.deep_learning.layers import (
+    ConvolutionTwoD, Activation, DropOut, BatchNormalization, Dense, Flatten)
+from ..deep_learning.grad_optimizers import Adam
+
 
 class DCGAN:
     """
@@ -18,3 +24,37 @@ class DCGAN:
 
         self.build_discriminator(optimizer, loss_function)
         self.build_gen(optimizer, loss_function)
+
+    def build_disctiminator(self, optimizer, loss_function
+                            ):
+        """
+            Creates the network discriminator
+        """
+        model = Neural_Network(optimizer=optimizer, loss=loss_function)
+
+        model.add_layer(ConvolutionTwoD(no_of_filters=32,
+                                        filter_shape=(3, 3),
+                                        stride=2, input_shape=self.img_shape))
+        model.add_layer(Activation('leaky_relu'))
+        model.add_layer(DropOut(p=.25))
+        model.add_layer(ConvolutionTwoD(64, filter_shape=(3, 3), stride=2))
+        model.add_layer(ZerosPadding2D(padding=((0, 1), (0, 1))))
+        model.add_layer(Activation('reaky_relu'))
+        model.add_layer(DropOut(.25))
+
+        model.add_layer(BatchNormalization(momentum=.8))
+        model.add_layer(ConvolutionTwoD(128,
+                                        filter_shape=(3, 3), stride=2))
+        model.add_layer(Activation('reaky_relu'))
+        model.add_layer(DropOut(0.25))
+
+        model.add_layer(BatchNormalization(momentum=.8))
+        model.add_layer(ConvolutionTwoD(256, filter_shape=(3, 3),
+                                        stride=1))
+        model.add_layer(Activation('reaky_relu'))
+        model.add_layer(DropOut(0.25))
+        model.add_layer(Flatten())
+        model.add_layer(Dense(units=2))
+        model.add_layer(Activation('softmax'))
+
+        return model
