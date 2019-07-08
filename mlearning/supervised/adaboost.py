ae023087aa92672ac5c2496fc1c8023d19693f78
mugoh <mugoh.ks@gmail.com>
2019-07-08 23:58:02 +0300

    (adb): create decision stump

--- mlearning/supervised/adaboost.py
+++ mlearning/supervised/adaboost.py
@@ -5,6 +5,9 @@
 """
 import numpy as np
 
+from dataclasses import dataclass
+from typing import Any
+
 
 class AdaBoost:
     """
@@ -12,12 +15,13 @@ class AdaBoost:
 
         Parameters:
         ----------
-        classifiers: int
+        n_classifiers: int
             Number of weak classifiers to used
     """
 
-    def __init__(self, classifiers):
-        self.classifiers = classifiers
+    def __init__(self, n_classifiers):
+        self.n_classifiers = n_classifiers
+        self.classifiers = []
 
     def fit(self, X, y):
         """
@@ -25,3 +29,32 @@ class AdaBoost:
         """
 
         self.n_samples, self.n_feat = X.shape
+
+        # Init weights to 1/n
+        weights = np.full(self.n_samples, 1 / self.n_samples)
+
+        for i in range(self.n_classifiers):
+            clf = DecisionStump()
+
+
+@dataclass
+class DecisionStump:
+    """
+        Weak classifier for Adaboost
+
+        Parameters
+        ----------
+        polarity: int
+            Determines classification of sample as -1 or 1
+            from threshold
+        aplha: float
+            Indicates classifiers accuracy
+        feature_idx: int
+            Index of feature used in making the classification
+        threshold: int
+            Threshold value against which feature is measured against
+    """
+    polarity: int = 1
+    alpha: float = .02
+    feature_idx: Any = None
+    threshold: Any = None
