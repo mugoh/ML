232fdaf10c22163abfe01c0a22e93573a359aded
mugoh <mugoh.ks@gmail.com>
2019-07-09 22:48:25 +0300

    (adb): reconfigure samples from error

--- mlearning/supervised/adaboost.py
+++ mlearning/supervised/adaboost.py
@@ -35,6 +35,41 @@ class AdaBoost:
 
         for i in range(self.n_classifiers):
             clf = DecisionStump()
+            min_error = float('inf')
+
+            # Find best threshold for y prediction
+            for feat_idx in range(self.n_feat):
+                feature_val = np.expand_dims(X[:, feat_idx], axis=1)
+                uniques = np.unique(feature_val)
+
+                for threshold in uniques:
+                    self.p = 1
+
+                    prediction = np.ones(y.shape)
+                    # Sample values below index. Label -1
+                    prediction[X[:, feat_idx] < threshold] = -1
+
+                    self.error = np.sum(weights[y != prediction])
+
+                    new_min_err = self._reclassify_on_error(
+                        min_error)
+                    if new_min_err:
+                        min_error = self.error
+                        clf.polarity = self.p
+                        clf.threshold = threshold
+                        clf.feature_idx = feat_idx
+
+    def reclassify_on_error(self, min_err):
+        """
+            Flips polarity of samples based on error value
+        """
+        it_happens = self.error > .5
+
+        if it_happens:
+            self.error = 1 - err
+            self.p = -1
+
+        return False if not self.error < min_err else True
 
 
 @dataclass
